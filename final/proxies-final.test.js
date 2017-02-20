324870099ee64f804d5c488ebe157ac69031fd1f
Khaled Garbaya <khaled@contentful.com>
2017-02-20 20:40:43 +0100

    Updated tests

--- final/proxies-final.test.js
+++ final/proxies-final.test.js
@@ -1,113 +1,85 @@
+/* eslint-disable */
 
-/* global test expect */
-
-test('Returns a default value if the property does not exist', () => {
-  const handler = {
-    get: (target, name) => {
-      return name in target ? target[name] : 37
-    }
+test('private property using get, set, has and deleteProperty traps', () => {
+  const target = {
+    _id: 'klsjdahflkastfaskfdg',
+    age: 29,
+    name: 'Khaled'
   }
-  const target = {}
-  const p = new Proxy(target, handler)
-  p.a = 1
-  p.b = undefined
-  expect(p.a).toBe(1)
-  expect(p.b).toBe(undefined)
-  expect('c' in p).toBe(false)
-  expect(p.c).toBe(37)
-})
-
-test('Throws an exception if age is invalid', () => {
   const handler = {
-    set: (target, prop, value) => {
-      if (prop === 'age') {
-        if (!Number.isInteger(value)) {
-          throw new TypeError('invalid age')
-        }
-        target[prop] = value
-        return true // this is important
+    get: (target, prop) => {
+      if (prop[0] === '_') {
+        throw new TypeError('Access denied')
       }
+      return target[prop]
+    },
+    set: (target, prop, value) => {
+      if (prop[0] === '_') {
+        throw new TypeError('Access denied')
+      }
+      target[prop] = value
+      return true
     }
+
   }
-  const target = {}
   const proxy = new Proxy(target, handler)
-  expect(() => { proxy.age = 'invalid' }).toThrow()
-  proxy.age = 29
-  expect(proxy.age).toBe(29)
+  expect(() => {proxy._id}).toThrow()
+  expect(() => {proxy._id = 'something'}).toThrow()
+  expect(() => {proxy.age}).not.toThrow()
+  expect( proxy.age ).toBe(29)
 })
 
-test('Easy Access for some properties', () => {
+test('migrate object with backward compatibility using get, set and has traps', () => {
+  const target = {
+    personalInfos: {
+      age: 29,
+      name: 'Khaled',
+      email: 'bla@gmail.com'
+    },
+    userGroup: 'Nodejs'
+  }
   const handler = {
     get: (target, prop) => {
       if (prop in target.personalInfos) {
         return target.personalInfos[prop]
       }
-      return target[prop]
-    }
-  }
-  const target = {
-    personalInfos: {
-      firstname: 'Jhon',
-      lastname: 'Doe',
-      age: 29
-    },
-    id: 1234
-  }
-  const proxy = new Proxy(target, handler)
-  expect(proxy.personalInfos.age).toBe(29)
-  expect(proxy.id).toBe(1234)
-  expect(proxy.age).toBe(29)
-})
 
-test('No Access for private member', () => {
-  const handler = {
+      return target[prop]
+    },
     set: (target, prop, value) => {
-      if (prop[0] === '_') {
-        throw new TypeError(`invalid set to a private member ${prop}`)
+      if (prop in target.personalInfos) {
+        target.personalInfos[prop] = value
+        return true
       }
+
       target[prop] = value
       return true
-    },
-    has: (target, prop) => {
-      if (prop[0] === '_') {
-        return false
-      }
-      return prop in target
+
     }
   }
-  const target = {
-    personalInfos: {
-      firstname: 'Jhon',
-      lastname: 'Doe',
-      age: 29
-    },
-    _id: 1234
-  }
   const proxy = new Proxy(target, handler)
-  proxy.a = 'b'
-  expect(proxy.a).toBe('b')
-  expect('_id' in proxy).toBe(false)
-  expect('personalInfos' in proxy).toBe(true)
-  expect(() => { proxy._id = 9876 }).toThrow()
+  expect(proxy.age).toBe(proxy.personalInfos.age)
+  proxy.age = 30
+  expect(proxy.personalInfos.age).toBe(30)
+  expect(proxy.userGroup).toBe('Nodejs')
+  proxy.userGroup = 'NodeMeetup'
+  expect(proxy.userGroup).toBe('NodeMeetup')
 })
-
-test('Bonus, python like array', () => {
+test('Bonus: Python like array, if the index is -1 return last item', () => {
+  const target = []
   const handler = {
     get: (target, prop) => {
       if (prop in target) {
         return target[prop]
       }
       const i = parseInt(prop)
-      if (!isNaN(i) && i < 0 && Math.abs(i) <= target.length) {
-        return target[target.length + i]
-      }
+      return target[target.length + i]
     }
   }
-  let proxiedArray = new Proxy([], handler)
-
-  proxiedArray.push('first', 'second', 'third')
-
-  expect(proxiedArray[0]).toBe('first')
-  expect(proxiedArray[-1]).toBe('third')
+  const proxy = new Proxy(target, handler)
+  proxy.push('first', 'second', 'third')
+  expect(proxy[0]).toBe('first')
+  expect(proxy[-1]).toBe('third')
+  expect(proxy[-2]).toBe('second')
 })
 
